641cb5ba62be861b76d996c30e4b8dddd70916ba
AndyAP7 <97900508+AndyAP7@users.noreply.github.com>
2023-11-02 14:19:27 -0400

    Week 9

---
+++ Week9.ps1
@@ -0,0 +1,9 @@
+﻿ipconfig /all | Select-String -Pattern "DHCP Server"
+ipconfig /all | Select-String -Pattern "DNS Server"
+Get-Process | Select-Object ProcessName, Path, ID | export-Csv -NoTypeInformation `
+-Path "C:\Users\andy.phuong\Desktop\processes.csv" 
+Get-Service | Where { $_.Status -eq "Running" } | Select-Object DisplayName, Status |export-Csv -NoTypeInformation `
+-Path "C:\Users\andy.phuong\Desktop\services.csv" 
+Start-Process -FilePath "C:Windows\System32\calc.exe"
+sleep 2
+Stop-Process -Name CalculatorApp
