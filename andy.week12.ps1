b5dea585956d60490610fccd916497528d68d51d
AndyAP7 <97900508+AndyAP7@users.noreply.github.com>
2023-11-30 14:08:55 -0500

    Add files via upload

---
+++ andy.week12.ps1
@@ -0,0 +1,16 @@
+﻿#Login to a remote SSH Server
+New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
+
+while ($True) {
+#Prompt to run commmands
+$the_cmd = read-host -Prompt "Please enter a command"
+
+#Run command on SSH Server
+(Invoke-SSHCommand -index 0 $the-cmd).Output
+
+}
+Set-SCPFile -Computername '192.168.4.22' -Credential (Get-Credential sys320)
+-RemotePath '/home/sys320' -LocalFile '.\tedx.jpeg'
+
+#Close SSH Session
+Remove-SSHSession -SessionId 
